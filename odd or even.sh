5a05c4a6eb08ace2aa42c9ed64d253d1d60a2898
PKanishk <123526205+PKanishk@users.noreply.github.com>
2023-05-08 10:24:23 +0530

    Add files via upload

---
+++ odd or even.sh
@@ -0,0 +1,9 @@
+echo "welcome boss"                                                                                                                                                                                                                                                              echo "odd or even"
+echo " enter a number"
+read n 
+if(let n%2==0)
+then 
+echo "$n is even number"
+else 
+echo "$n is odd number"
+fi
